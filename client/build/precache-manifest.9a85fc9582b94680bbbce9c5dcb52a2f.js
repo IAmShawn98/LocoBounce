9fbd7f18f1e32e1f0c21c3717badd15722c35edb
icepants <shawnluther99@gmail.com>
2019-10-29 14:49:09 -0400

    Fixing issue with heroku deploy.

---
+++ client/build/precache-manifest.9a85fc9582b94680bbbce9c5dcb52a2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37bf440a6c21f493db88b119a6d4c4e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a24710a5d18bf8345a52",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "9176d66bcb8eff8fbe10",
+    "url": "/static/css/main.0423efd7.chunk.css"
+  },
+  {
+    "revision": "a24710a5d18bf8345a52",
+    "url": "/static/js/2.430515e7.chunk.js"
+  },
+  {
+    "revision": "9176d66bcb8eff8fbe10",
+    "url": "/static/js/main.c18a1621.chunk.js"
+  },
+  {
+    "revision": "c18ec84a3b55c998c1c2",
+    "url": "/static/js/runtime~main.1638fc71.js"
+  }
+]);
